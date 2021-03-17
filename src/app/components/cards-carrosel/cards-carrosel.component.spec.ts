a2832ed61eeec793b95f617f6b10b2249424fca4
VictorSouza_ <victorsouza0106@gmail.com>
2021-03-17 11:22:27 -0300

    Erase node Module

--- src/app/components/cards-carrosel/cards-carrosel.component.spec.ts
+++ src/app/components/cards-carrosel/cards-carrosel.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CardsCarroselComponent } from './cards-carrosel.component';
 
@@ -6,7 +6,7 @@ describe('CardsCarroselComponent', () => {
   let component: CardsCarroselComponent;
   let fixture: ComponentFixture<CardsCarroselComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CardsCarroselComponent ]
     })
